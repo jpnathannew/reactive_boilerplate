6463485303b5d77465bea6c7c0fc359f29315ff8
Padmanathan <padmanathan@digital.datamatics.com>
2021-03-05 14:21:29 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render} from 'react-dom';
 import './index.css';
-import App from './App';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import RootReducer from "./core-files/reducers"
+import thunk from "redux-thunk";
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk)
+  // other store enhancers if any
+);
+
+const store = createStore(RootReducer, enhancer);
+const persistor = persistStore(store);
+
+render(
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export default store;
