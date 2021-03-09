43049e2e8c3ec4ef0f534426679b6202f6d0a862
Mohnish <vurity.mohnish@digital.datamatics.com>
2021-03-09 09:33:33 +0530

    eslint and enzyme configuration

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
+let wrapped = shallow(<App />);
+
+describe('Title', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+  it('should render the Title Component correctly', () => {   
+    expect(wrapped).toMatchSnapshot();
+  });
 });
